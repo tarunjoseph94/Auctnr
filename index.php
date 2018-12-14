bc45d6859209db2299ca3e8ccbce9df0e9678083
Tarun Joseph <tarunjoseph94@gmail.com>
2018-12-14 15:14:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Trackr</title>
+	  <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<style>
+		.mt-15 {
+			margin-top: 15px !important; }
+
+		.mt-30 {
+			margin-top: 30px !important; }
+
+		.mt-50 {
+			margin-top: 50px !important; }
+
+		.mt-70 {
+			margin-top: 70px !important; }
+
+		.mt-100 {
+			margin-top: 100px !important; }
+
+		.mb-15 {
+			margin-bottom: 15px !important; }
+
+		.mb-30 {
+			margin-bottom: 30px !important; }
+
+		.mb-50 {
+			margin-bottom: 50px !important; }
+
+		.mb-70 {
+			margin-bottom: 70px !important; }
+
+		.mb-100 {
+			margin-bottom: 100px !important; }
+
+		.ml-15 {
+			margin-left: 15px !important; }
+
+		.ml-30 {
+			margin-left: 30px !important; }
+
+		.ml-50 {
+			margin-left: 50px !important; }
+
+		.mr-15 {
+			margin-right: 15px !important; }
+
+		.mr-30 {
+			margin-right: 30px !important; }
+
+		.mr-50 {
+			margin-right: 50px !important; }
+
+		/* Height */
+		.height-400 {
+			height: 400px !important; }
+
+		.height-500 {
+			height: 500px !important; }
+
+		.height-600 {
+			height: 600px !important; }
+
+		.height-700 	{
+			height: 700px !important; }
+
+		.height-800 {
+			height: 800px !important; }
+
+				.btn {
+		  			background: #000;
+				    color: #fff;
+				 		}
+				.trackr-nav ul li {
+		    list-style: none;
+				}
+				.trackr-nav ul li a {
+		    text-decoration: none;
+				}
+				.trackr-btn {
+		    display: inherit;
+				}
+				.header-area {
+		    margin-left: 50px;
+				margin-top: 50px;
+				}
+				.active.btn {
+		    background: #fff300;
+		    color: #000;
+				}
+				.trackr-btn-group {
+		    display: grid;
+		    width: 155px;
+
+			}
+			.red-bg {
+		    background: red;
+		    min-height: 500px;
+			}
+			.short-box {
+		    background-color: green;
+			}
+				.long-box
+				{
+					background-color: blue;
+				}
+				.long-box {
+		    height: 270px;
+		}
+		.short-box {
+		    height: 250px;
+		}
+		.short-box.card {
+    overflow: hidden;
+}
+.item-details {
+    position: absolute;
+    z-index: 5;
+    top: 0;
+    left: 0;
+}
+.item-img {
+    position: relative;
+  
+}
+		</style>
+	</head>
+
+	<body>
+		<div class="main-content container-fluid">
+			<div class="row">
+
+
+			<!-- Header Area Start -->
+			<div class="col-md-3">
+
+        <header class="header-area ">
+            <!-- Close Icon -->
+            <div class="nav-close">
+                <i class="fa fa-close" aria-hidden="true"></i>
+            </div>
+            <!-- Logo -->
+            <div class="logo">
+                <a href="index.html"><img src="images/core-img/logo.png" alt=""></a>
+            </div>
+            <!-- Amado Nav -->
+            <nav class="trackr-nav mt-30">
+                <ul>
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li><a href="short.html">short</a></li>
+
+                </ul>
+            </nav>
+            <!-- Button Group -->
+            <div class="trackr-btn-group mt-30 mb-100">
+                <a href="#" class="btn trackr-btn mb-15 ">Sign Up</a>
+                <a href="#" class="btn trackr-btn active">Login</a>
+            </div>
+
+        </header>
+			</div>
+        <!-- Header Area End -->
+				<div class="col-md-9">
+					<div class="red-bg">
+						<div class="container">
+
+						<div class="row">
+							<div class="card-columns">
+
+						<div class="short-box card">
+							<div class="item">
+								<div class="item-img">
+									<img src="images/product-img/pro-big-1.jpg" object-fit="contain" height="100%" width="100%" />
+							</div>
+							<div class="item-details">
+								<p>
+									From 180
+								</p>
+								<p>
+									Chair
+								</p>
+							</div>
+
+							</div>
+						</div>
+						<div class="long-box card">
+							<p>
+								Enter
+							</p>
+						</div>
+						<div class="short-box card">
+							<p>
+								Enter
+							</p>
+						</div>
+
+					<div class="long-box card">
+						<p>
+							Enter
+						</p>
+					</div>
+					<div class="short-box card">
+						<p>
+							Enter
+						</p>
+					</div>
+					<div class="long-box card">
+						<p>
+							Enter
+						</p>
+					</div>
+
+				<div class="short-box card">
+					<p>
+						Enter
+					</p>
+				</div>
+				<div class="long-box card">
+					<p>
+						Enter
+					</p>
+				</div>
+				<div class="card short-box ">
+					<p>
+						Enter
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+					</div>
+				</div>
+				</div>
+			</div>
+		</body>
+	<script src="js/bootstrap.min.js"></script>
+
+</html>
